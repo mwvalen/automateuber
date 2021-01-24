9c9d48761976bfdf85dd91a4374d93aadc661e13
Anthony kim <ankim90@gmail.com>
2021-01-24 17:43:39 -0500

    complete without if

--- uber-eats.js
+++ uber-eats.js
@@ -52,14 +52,12 @@ const windowSize = `--window-size=${windowWidth},${windowHeight}`;
   // await Promise.all([
   //   changelocation.click()
   //   ]); 
-  // const location = await page.waitForSelector('input[name="searchTerm"]', {
-  //   visible: true,
-  //   timeout: 3000
-  // });      // -->  <<Location>>
+  // await page.waitForTimeout(3000);
+  // const location = await page.waitForSelector('input[name="searchTerm"]');      // -->  <<Location>>
 
-  // //console.log("hello",location);
-  // await location.click();
-  // await location.type("CN Tower");
+  // // //console.log("hello",location);
+  // //await location.click();
+  // await location.keyboard.type("CN Tower");
   const searchBar = await page.waitForSelector('#search-suggestions-typeahead-input');  // --> <<Search Bar>>
   await searchBar.type("Pizza pizza");
   await Promise.all([
@@ -71,52 +69,70 @@ const windowSize = `--window-size=${windowWidth},${windowHeight}`;
     const elements = [...document.querySelectorAll('p')];
     const element = elements.find(element => element.innerHTML.toLowerCase().includes('pizza pizza'));
     element.click();
-  })
+  });
   await page.waitForTimeout(3000);
-  console.log('page 1', page)
   await page.evaluate(() => {
     const elements = [...document.querySelectorAll('button')];
     const element = elements.find(element => element.innerHTML.toLowerCase().includes('signature pizzas'));
-    element.click();
-  })
-  console.log('mwv -1')
-  await page.waitForTimeout(3000);
-  console.log('mwv -2')
-  console.log('page', page)
-  await page.evaluate(() => {
-    console.log('mwv 0')
-    const elements = [...document.querySelectorAll('h2')];
-    console.log('mwv 1')
-    const element = elements.find(element => element.innerHTML.toLowerCase().includes('signature pizzas'));
-    console.log('mwv 2')
-    const signaturePizzasUl = element.nextSibling;
-    console.log('mwv 3')
-    const signaturePizzasLis = signaturePizzasUl.children;
-    console.log('mwv 4')
-    console.log('signature pizza lis', signaturePizzasLis);
-    // element.click();
-  })
+    if (element === undefined) {
+      page.evaluate(() => {
+        const elements = [...document.querySelectorAll('button')];
+        const element = elements.find(element => element.innerHTML.toLowerCase().includes('more'));
+        element.click();
+      });
+      page.waitForTimeout(2000);
+      page.evaluate(() => {
+        const elements = [...document.querySelectorAll('div.f6.nd.ba.lk.lj.ne')];
+        const element = elements.find(element => element.innerHTML.toLowerCase().includes('signature pizzas'));
+        element.click();
+      });      
+      } else {
+      element.click();
+      };
+  });
 
-  // const signaturePizzas = await page.waitForSelector('#main-content > div.b7.b8.b9.ba.bb > div:nth-child(2) > div > div > div > div > nav > div:nth-child(6) > button');  // --> <<Signature Pizzas>>
-  // signaturePizzas.click();
-  // await page.waitForTimeout(1000);
-  // const thirdChoice = await page.waitForSelector('#main-content > div.b7.b8.b9.ba.bb > ul > li:nth-child(6) > ul > li:nth-child(3) > div > div > div > div.ag.cu.ah.cw > div.c9 > h4 > div');  // --> <<Third Choice>>
-  // thirdChoice.click();
-  // await page.waitForTimeout(1000);
-  // await page.click("")
-  // //const large = await page.waitForSelector('input[name="9dc2ba7f-4dd3-5cd8-a250-7e14fa66d049+0"]');   // -->  <<Large>>
-  // const large = await page.waitForSelector('#wrapper > div:nth-child(6) > div > div > div.b9.af.bd > div.af.mp > ul > li:nth-child(1) > div > div.c4 > label:nth-child(8) > div.cu.ag.e4.k8 > div.ag.cu.ah.g5.n0 > div > div.ca.cb.cc');   // -->  <<Large>>
-  // large.click();
-  // const addOrder = await page.waitForSelector('#wrapper > div:nth-child(6) > div > div > div.b9.af.bd > div.af.mp > div.ag.be.bf.e4.c4.em.dr.n4 > button'); //--> <<Add Order>>
-  // await Promise.all([
-  //   addOrder.click(),
-  //   page.waitForNavigation({waitUntil:'networkidle2'})
-  //   ]);
-  // const checkout = await page.waitForSelector('#wrapper > div.it.af > header > div.b7.b8.b9.ba.bb.bc.bd > div > div.dg.ag.dh.bf > div.bs.nn.no.np.bk.bd.de.nq.l2 > div > div.nt > div.c4.ai.dr.bu.d9.n4 > a'); // --> <<Checkout>>
-  // await Promise.all([
-  //   checkout.click(),
-  //   page.waitForNavigation({waitUntil:'networkidle2'})
-  //   ]);
-  
+  // await page.waitForTimeout(3000);
+  // await page.evaluate(() => {
+  //   console.log('mwv 0')
+  //   const elements = [...document.querySelectorAll('h2')];
+  //   console.log('mwv 1')
+  //   const element = elements.find(element => element.innerHTML.toLowerCase().includes('signature pizzas'));
+  //   console.log('mwv 2')
+  //   const signaturePizzasUl = element.nextSibling;
+  //   console.log('mwv 3')
+  //   const signaturePizzasLis = signaturePizzasUl.children;
+  //   console.log('mwv 4')
+  //   console.log('signature pizza lis', signaturePizzasLis);
+  //   // element.click();
+  // })
+
+  await page.waitForTimeout(1000);
+  const thirdChoice = await page.waitForSelector('#main-content > div.b7.b8.b9.ba.bb > ul > li:nth-child(6) > ul > li:nth-child(3) > div > div > div > div.ag.cu.ah.cw > div.c9 > h4 > div');  // --> <<Third Choice>>
+  thirdChoice.click();
+  await page.waitForTimeout(3000);
+  await page.evaluate(() => {
+    const elements = [...document.querySelectorAll('div.ca.cb.cc')];
+    const element = elements.find(element => element.innerHTML.toLowerCase().includes('large'));
+    element.click();
+  });
+  await page.waitForTimeout(3000);
+  await page.evaluate(() => {
+    const elements = [...document.querySelectorAll('div.cu.an')];
+    const element = elements.find(element => element.innerHTML.toLowerCase().includes('to order'));
+    element.click();
+  });
+  await page.waitForTimeout(3000);
+  await page.evaluate(() => {
+    const elements = [...document.querySelectorAll('a.ag.e4.bf.lp.cw')];
+    const element = elements.find(element => element.innerHTML.toLowerCase().includes('checkout'));
+    element.click();
+  });
+  await page.waitForTimeout(3000);
+  await page.evaluate(() => {
+    const elements = [...document.querySelectorAll('div.cv.cp.cc.l9.ba')];
+    const element = elements.find(element => element.innerHTML.toLowerCase().includes('15%'));
+    element.click();
+  });
+
 })();
 
--- whatsapp.js
+++ whatsapp.js
@@ -1,10 +1,18 @@
 const puppeteer = require('puppeteer');
 
+const fullscreen = false;
+
+const windowWidth=1400;
+const windowHeight=1080;
+
+const windowSize = `--window-size=${windowWidth},${windowHeight}`;
+
 (async () => {
-  const browser = await puppeteer.launch({ headless: false, defaultViewport: null, args: ['--start-fullscreen'] });  // --> <<Browser>>
+  let launchArgs = ''
+  launchArgs += windowSize
+  if (fullscreen) launchArgs += '--start-fullscreen'
+  const browser = await puppeteer.launch({ headless: false, defaultViewport: null, args: [launchArgs] });  // --> <<Browser>>
   const page = await browser.newPage();  // --> <<Page>>
-  page.setDefaultNavigationTimeout(0);
-  page.setDefaultTimeout(0);
   await page.goto('https://web.whatsapp.com/')
   const firstPane = await page.waitForSelector('#pane-side > div:nth-child(1) > div > div > div:nth-child(1)');  //  -->  <<First Pane>>
   console.log("hello");
