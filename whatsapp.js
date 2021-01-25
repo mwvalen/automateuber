6c21c8a919ba1edbae0968ec34bac1e66b6a833e
Anthony kim <ankim90@gmail.com>
2021-01-24 19:32:23 -0500

    complete with errors

--- whatsapp.js
+++ whatsapp.js
@@ -13,8 +13,27 @@ const windowSize = `--window-size=${windowWidth},${windowHeight}`;
   if (fullscreen) launchArgs += '--start-fullscreen'
   const browser = await puppeteer.launch({ headless: false, defaultViewport: null, args: [launchArgs] });  // --> <<Browser>>
   const page = await browser.newPage();  // --> <<Page>>
-  await page.goto('https://web.whatsapp.com/')
-  const firstPane = await page.waitForSelector('#pane-side > div:nth-child(1) > div > div > div:nth-child(1)');  //  -->  <<First Pane>>
-  console.log("hello");
-  await firstPane.click();
+  await page.goto('https://web.whatsapp.com/');
+  const newChat = await page.waitForSelector('div[title="New chat"]', {
+    visible: true,
+  });                                       // --> <<New Chat>>
+  await newChat.click();
+  const name = await page.waitForSelector('div[dir="ltr"]');    // --> <<Search Name>>
+  await name.type("Michael Valentine");
+  await page.waitForTimeout(3000);
+  await page.evaluate(() => {
+    const elements = [...document.querySelectorAll('div._1MZWu')];
+    const element = elements.find(element => element.innerHTML.toLowerCase().includes('michael valentine'));
+    element.click();
+  });
+  await page.waitForTimeout(3000);
+  const typeMessage = await page.waitForSelector('div._1awRl"]');    // --> <<Type Message>>
+  await typeMessage.type("testing");
+  await page.keyboard.press('Enter');
+  
+    
+  // while (i = undefined) {
+  //   const newChat = await page.waitForSelector('div[title="New chat"]');    // --> <<New Chat>>
+  //   i = newChat };
+  // console.log("hello");
 })();
