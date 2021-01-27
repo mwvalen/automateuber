843fdf3cebc70006dc725439ac9cbde064b6d635
mwvalen <mwillvalentine@gmail.com>
2021-01-26 22:32:35 -0800

    clean up ubereats

--- uber-eats.js
+++ uber-eats.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer');
 
+// SETTINGS ******************************************************************************************** //
+
 const launchSettings = {
   fullscreen: false,
   windowWidth: 1400,
@@ -23,11 +25,14 @@ const selectors = {
   usernameNextButtonSelector: 'button[class="btn btn--arrow btn--full"]',
   passwordSelector: 'input[name="password"]',
   submitPasswordButton: 'button[class="btn btn--arrow btn--full push--top"]',
-  foodSearchBar: '#search-suggestions-typeahead-input',
-  thirdChoice: '#main-content > div.b7.b8.b9.ba.bb > ul > li:nth-child(6) > ul > li:nth-child(3) > div > div > div > div.ag.cu.ah.cw > div.c9 > h4 > div'
+  foodSearchBar: '#search-suggestions-typeahead-input'
 }
 
 launchSettings['windowSize'] = `--window-size=${launchSettings.windowWidth},${launchSettings.windowHeight}`;
+
+// ********************************************************************************************************* //
+
+
 
 (async () => {
   const page = await openAndNavigateTo('https://www.ubereats.com/', launchSettings);
@@ -35,12 +40,14 @@ launchSettings['windowSize'] = `--window-size=${launchSettings.windowWidth},${la
   await orderPizza(orderDetails, page);
 })();
 
+
+
 async function openAndNavigateTo (url, settings) {
   let launchArgs = '';
-  launchArgs += settings.windowSize;  // --> 
+  launchArgs += settings.windowSize;
   if (settings.fullscreen) launchArgs += '--start-fullscreen';
-  const browser = await puppeteer.launch({ headless: false, defaultViewport: null, args: [launchArgs] });  // --> <<Browser>>
-  const page = await browser.newPage();  // --> <<Page>>
+  const browser = await puppeteer.launch({ headless: false, defaultViewport: null, args: [launchArgs] });
+  const page = await browser.newPage();
   await page.goto(url);
   return page;
 };
@@ -54,7 +61,7 @@ async function logIn ({ username, password }, page) {
   const usernameInput = await page.waitForSelector(selectors.usernameSelector);    // --> <<Username>>
   await usernameInput.type(username);
   await page.waitForTimeout(1000);
-  const nextButton = await page.waitForSelector(selectors.usernameNextButtonSelector);                                                                 // -->  <<Username Button>>
+  const nextButton = await page.waitForSelector(selectors.usernameNextButtonSelector);     // -->  <<Username Button>>
   await Promise.all([
     await nextButton.click(),
     page.waitForNavigation({waitUntil:'networkidle2'})
@@ -62,7 +69,7 @@ async function logIn ({ username, password }, page) {
   const passwordInput = await page.waitForSelector(selectors.passwordSelector);     // -->  <<Password>>
   await passwordInput.type(password);
   await page.waitForTimeout(1000);
-  const passwordButton = await page.waitForSelector(selectors.submitPasswordButton);             // --> <<Password Button>>
+  const passwordButton = await page.waitForSelector(selectors.submitPasswordButton);   // --> <<Password Button>>
   await Promise.all([
     passwordButton.click(),
     page.waitForNavigation({waitUntil:'networkidle2'})
@@ -70,79 +77,57 @@ async function logIn ({ username, password }, page) {
 }
 
 async function orderPizza ({ restaurant, size, tip }, page) {
-  const searchBar = await page.waitForSelector(selectors.foodSearchBar);  // --> <<Search Bar>>
-  await searchBar.type(restaurant);
-  await Promise.all([
-    page.keyboard.press('Enter'),
-    page.waitForNavigation({ waitUntil: 'networkidle2' }),
-  ]);
-  await page.waitForTimeout(3000);
-  await page.evaluate((restaurant) => {
-    const elements = [...document.querySelectorAll('p')];
-    const element = elements.find(element => element.innerHTML.toLowerCase().includes(restaurant));
-    element.click();
-  },restaurant);
-  await page.waitForTimeout(3000);
-  await page.evaluate(() => {
-    const elements = [...document.querySelectorAll('button')];
-    const element = elements.find(element => element.innerHTML.toLowerCase().includes('signature'));
-    element.click();
-  });
-  await page.waitForTimeout(3000);
-  const pizzaChoice = await page.waitForSelector(selectors.thirdChoice);  // --> <<Third Choice>>
-  pizzaChoice.click();
-  await page.waitForTimeout(3000);
-  await page.evaluate((size) => {
-    const elements = [...document.querySelectorAll('label')];
-    const element = elements.find(element => element.innerHTML.toLowerCase().includes(size));
-    element.click();
-  },size);
-  await page.waitForTimeout(3000);
-  await page.evaluate(() => {
-    const elements = [...document.querySelectorAll('button')];
-    const element = elements.find(element => element.innerHTML.toLowerCase().includes('to order'));
-    element.click();
-  });
-  await page.waitForTimeout(4000);
-  await page.evaluate(() => {
-    const elements = [...document.querySelectorAll('a[rel="nofollow"]')];
-    const element = elements.find(element => element.innerHTML.toLowerCase().includes('checkout'));
-    element.click();
-  });
-  await page.waitForTimeout(3000);
-  await page.evaluate((tip) => {
-    const elements = [...document.querySelectorAll('div[role="radio"]')];
-    const element = elements.find(element => element.innerHTML.toLowerCase().includes(tip));
-    element.click();
-  },tip);
+  try {
+    const searchBar = await page.waitForSelector(selectors.foodSearchBar);  // --> <<Search Bar>>
+    await searchBar.type(restaurant);
+    await Promise.all([
+      page.keyboard.press('Enter'),
+    ]);
+    let element = await getElementByInnerHtml('p', restaurant, page)  // --> <<Chosen Restaurant>>
+    await page.evaluate(e => e.click(), element);
+
+    element = await getElementByInnerHtml('button', 'signature pizzas', page) // --> <<Signature Pizzas>>
+    await page.evaluate(e => e.click(), element);
+
+    element = await getElementByInnerHtml('h2', 'signature pizzas', page) 
+    await page.evaluate(e => {                                                // --> <<Third Choice>>
+      const sigPizzasList = e.nextSibling;
+      const chosenItem = sigPizzasList.children[2];
+      chosenItem.children[0].click()
+    }, element);
+
+    element = await getElementByInnerHtml('label', size, page)  // --> <<Size>>
+    await page.evaluate(e => e.click(), element);
+
+    element = await getElementByInnerHtml('button', 'to order', page)  // --> <<Order Button>>
+    await page.evaluate(e => e.click(), element);
+
+    await page.waitForTimeout(2000);
+    await page.evaluate(() => window.location.href="/checkout");   // --> <<Go To Checkout>>
+
+    await page.waitForTimeout(1000);
+    element = await getElementByInnerHtml('div[role="radio"]', tip, page) // --> <<Tip>>
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-  // const locationButton = await page.waitForSelector('a[class="ca cb cc bc cd ce cf cg ch ag be bf bj ci cj ck ba"]', {
-  //   visible: true,
-  // });                                    //  --> <<Location Button>>
-  // await locationButton.click();    
-  // const changelocation = await page.waitForSelector('a[class="bc cd ce cf cg ch ca cb cc ag io cj ka bf ba g5 d1"]');                                 //  --> <<Change Location Button>>
-  // await Promise.all([
-  //   changelocation.click()
-  //   ]); 
-  // await page.waitForTimeout(3000);
-  // const location = await page.waitForSelector('input[name="searchTerm"]');      // -->  <<Location>>
 
-  // // //console.log("hello",location);
-  // //await location.click();
-  // await location.keyboard.type("CN Tower");
-                //******************************** */
-  // await page.waitForTimeout(3000);
-  // await page.evaluate(() => {
-  //   console.log('mwv 0')
-  //   const elements = [...document.querySelectorAll('h2')];
-  //   console.log('mwv 1')
-  //   const element = elements.find(element => element.innerHTML.toLowerCase().includes('signature pizzas'));
-  //   console.log('mwv 2')
-  //   const signaturePizzasUl = element.nextSibling;
-  //   console.log('mwv 3')
-  //   const signaturePizzasLis = signaturePizzasUl.children;
-  //   console.log('mwv 4')
-  //   console.log('signature pizza lis', signaturePizzasLis);
-  //   // element.click();
-  // })
+// HELPERS
+
+async function getElementByInnerHtml (selector, value, page) {
+  let e = await page.evaluateHandle(async ({ selector, value }) => {
+      let element = null; 
+      let waitTime = 0;
+      while (!element && waitTime < 10000) {
+        console.log(`loop ${waitTime / 500}`);
+        await new Promise(res => setTimeout(res, 500));
+        waitTime += 500
+        const elements = [...document.querySelectorAll(selector)];
+        element = elements.find(element => element.innerHTML.toLowerCase().includes(value));
+      }
+      // console.log({ element, waitTime })
+      return element;
+  }, { selector, value })
+  return e
+}
